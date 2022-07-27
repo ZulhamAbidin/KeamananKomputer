9441ac0228fad35b81fcb9c5e5e080d3ee0d03b5
ZulhamAbidin <zlhm378@gmail.com>
2022-07-27 13:03:17 +0700

    navbar edit

---
+++ assets/js/main.js
@@ -0,0 +1,56 @@
+// JS NAVBAR
+   
+      const btnnav = document.querySelector("button.mobile-menu-button");
+      const menunav = document.querySelector(".mobile-menu");
+
+
+      btnnav.addEventListener("click", () => {
+        menunav.classList.toggle("hidden");
+      });
+  
+//JS KONTEN 1
+
+   
+      const  btnp1 = document.querySelector(".buttonp1");
+      const  kontenp1 = document.querySelector(".p1");
+
+
+      btnp1.addEventListener("click", () => {
+        kontenp1.classList.toggle("hidden");
+      });
+
+
+//JS KONTEN 2
+
+  
+      const  btnp2 = document.querySelector(".buttonp2");
+      const  kontenp2 = document.querySelector(".p2");
+
+
+      btnp2.addEventListener("click", () => {
+        kontenp2.classList.toggle("hidden");
+      });
+  
+
+// JS KONTEN 3 
+
+   
+      const  btnp3 = document.querySelector(".buttonp3");
+      const  kontenp3 = document.querySelector(".p3");
+
+
+      btnp3.addEventListener("click", () => {
+        kontenp3.classList.toggle("hidden");
+      });
+ 
+// JS KONTEN 4 
+
+  
+      const  btnp4 = document.querySelector(".buttonp4");
+      const  kontenp4 = document.querySelector(".p4");
+
+
+      btnp4.addEventListener("click", () => {
+        kontenp4.classList.toggle("hidden");
+      });
+   
